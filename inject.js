13da127d0fccff5300d945f3bc4ead7e0dd5b68e
Jay Kanakiya <kanakiyajay@yahoo.com>
2012-11-03 15:44:18 +0530

    Images are saved and added to the bottom

--- inject.js
+++ inject.js
@@ -1,19 +1,56 @@
-//** Content script doesnt work Offline ** , therefor use content Script
-console.log('Inject Script Working');
-var storage = chrome.storage.local;
-//If Online then Store it
+jQuery(document).ready(function  ($) {	
+	var url = window.location.href;//get the url of the page
+	var storage = chrome.storage.local;//Cache
+	console.log('Inject Script Working');
+	var storageObject = {};
+	//Store the html text
+	var theText = $('body').html().toString();
+	var imgArray = $('img');//get all the images in array
+                              var storeImgArray = []; 
+                                for (var i = 0; i < imgArray.length; i++) {
+                		//if () {}; //If image is from another domain
+                                    getBase64Image(imgArray[i],i);//Get the base64 string of the image
+                                    console.log(storeImgArray);
+                                };
+	//Also store in chrome storage
+	var itemStorage = {
+	            'html' : theText,
+	            'img' : storeImgArray
+	        };
 
-var theText = $('body').html().toString();
-//Also store in chrome storage
-var storageObject = {}
-var url = window.location.href;//get the url of the page
+	storageObject[url]=JSON.stringify(itemStorage);;//Mapping
 
-storageObject[url]=theText;//Mapping
+	 storage.set(storageObject, function() {
+	    // Notify that we saved.
+	    console.log('saved the page');
+	  });
+	storage.get(url, function(items) {  
+	  console.log(items);//Check whether stored
+	});
+            function getBase64Image(img,number) {
+                // Create an empty canvas element
+                var canvas = document.createElement("canvas");
+                canvas.width = img.width;
+                canvas.height = img.height;
+                console.log(canvas);
 
- storage.set(storageObject, function() {
-    // Notify that we saved.
-    console.log('saved the page');
-  });
-storage.get(url, function(items) {  
-  console.log(items);//Check whether stored
+                // Copy the image contents to the canvas
+                var ctx = canvas.getContext("2d");
+                ctx.drawImage(img, 0, 0);
+
+
+                // Get the data-URL formatted image
+                // Firefox supports PNG and JPEG. You could check img.src to
+                // guess the original format, but be aware the using "image/jpg"
+                // will re-encode the image.
+
+                //To avoid cross-domain errors
+                try{
+                	storeImgArray[i] = canvas.toDataURL("image/png");
+                } catch (error){
+                	console.log(error);
+                }
+                $('canvas').remove();
+                //return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+            }
 });
--- redirect.js
+++ redirect.js
@@ -5,6 +5,13 @@ storage.get('toAccessPage', function(items) {
 	var url = items.toAccessPage;//the Url content to fetch
 	storage.get(url,function  (items2) {
 		var content = items2[url];
-		$('body').html(content);
+		content = JSON.parse(content);
+		//console.log(content);
+		var theText = content.html;
+		var storeImgArray = content.img;
+		for (var i = 0; i < storeImgArray.length; i++) {
+			theText = theText +'<img src="'+storeImgArray[i]+'"></img>';
+		};
+		$('body').html(theText);
 	});
 });
